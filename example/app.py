650ba82c8e08912918a007bd31365099883af31d
Daniel Davies <d.w.davies@imperial.ac.uk>
2022-04-08 10:45:44 +0100

    Add simple dash app

---
+++ example/app.py
@@ -0,0 +1,42 @@
+# Simple Dash example from https://dash.plotly.com/layout
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+colors = {"background": "#111111", "text": "#7FDBFF"}
+
+df = pd.DataFrame(
+    {
+        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+        "Amount": [4, 1, 2, 2, 4, 5],
+        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+    }
+)
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors["background"],
+    paper_bgcolor=colors["background"],
+    font_color=colors["text"],
+)
+
+app.layout = html.Div(
+    style={"backgroundColor": colors["background"]},
+    children=[
+        html.H1(
+            children="Hello",
+            style={"textAlign": "center", "color": colors["text"]},
+        ),
+        html.Div(
+            children="Dash: A web application framework for your data.",
+            style={"textAlign": "center", "color": colors["text"]},
+        ),
+        dcc.Graph(id="example-graph-2", figure=fig),
+    ],
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
